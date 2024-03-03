8759cd6132ceb431e20ceeabc746880087cd3ff6
FawkesguyD <dgnitetskiy@gmail.com>
2024-03-04 02:42:08 +0300

    done 1-4 tasks

--- app/src/main/java/com/example/myapplication/DisplayActivity.java
+++ app/src/main/java/com/example/myapplication/DisplayActivity.java
@@ -2,11 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
 
+    @SuppressLint({"StringFormatMatches", "StringFormatInvalid"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,8 @@ public class DisplayActivity extends AppCompatActivity {
 
         TextView outputTextView = findViewById(R.id.textView3);
         outputTextView.setText(getString(R.string.label, text));
+
+        TextView sdk_version_view= findViewById(R.id.textView2);
+        sdk_version_view.setText(getString(R.string.label_sdk_version ,Build.VERSION.SDK_INT));
     }
 }
